a5e3d8f0926a5704a982bcef04d7e5bbad4b9a1a
Pranshu <pranshukk95@gmail.com>
2026-05-27 06:35:01 +0530

    Added RestClient and Feign implementation

---
+++ Additional/interservice/consumer/src/main/java/com/demo/consumer/controller/RestTemplateClient.java
@@ -0,0 +1,20 @@
+package com.demo.consumer.controller;
+
+import jakarta.annotation.Resource;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class RestTemplateClient {
+
+    private static final String PROVIDER_URL = "http://localhost:8081";
+    private final RestTemplate restTemplate;
+
+    public RestTemplateClient(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    public String getInstanceInfo() {
+        return restTemplate.getForObject(PROVIDER_URL + "/instance-info", String.class);
+    }
+}
